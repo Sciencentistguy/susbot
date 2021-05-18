939f7706c18011123ae936fd8aa76afb0a34a91e
Jamie Quigley <jamie@quigley.xyz>
2021-05-18 22:59:09 +0100

    sus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use log::*;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+use structopt::StructOpt;
+
+const APPROPRIATE_RESPONSE: &str = "<a:blacksus:844328240147333171><a:bluesus:844328240461774878><a:brownsus:844328240423239725><a:cyansus:844328240130424844><a:greensus:844328240351936573><a:limesus:844328240416161872><a:orangesus:844328240311304192><a:pinksus:844328240378281984><a:purplesus:844328240160309291><a:redsus:844328240285483008><a:whitesus:844328240222699550><a:yellowsus:844328240197664818>";
+
+
+const SUS_WORDS: [&str; 6] = ["sus", "vent", "impostor", "amogus", "amongus", "among us"];
+
+struct Handler;
+
+#[tokio::main]
+async fn main() {
+    // Set default log level to info unless otherwise specified.
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "susbot=info");
+    }
+    pretty_env_logger::init();
+    let opts = Opt::from_args();
+    let token = if opts.token.is_some() {
+        opts.token.unwrap()
+    } else if opts.token_filename.is_some() {
+        std::fs::read_to_string(opts.token_filename.unwrap()).expect("File does not exist")
+    } else {
+        env::var("DISCORD_TOKEN")
+            .expect("Expected either --token, --token-filename, or a token in the environment")
+    };
+
+    // Create a new instance of the Client, logging in as a bot. This will
+    // automatically prepend your bot token with "Bot ", which is a requirement
+    // by Discord for bot users.
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // Finally, start a single shard, and start listening to events.
+    //
+    // Shards will automatically attempt to reconnect, and will perform
+    // exponential backoff until it reconnects.
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.split(' ').any(|x| SUS_WORDS.contains(&x)) || msg.content.contains("among us") {
+            info!("SUS! <among us jingle plays>: author: {}; message: {}", msg.author.name, msg.content);
+            msg.reply_ping(&ctx, APPROPRIATE_RESPONSE).await.expect("Failed to reply to message");
+        }
+    }
+
+    // Set a handler to be called on the `ready` event. This is called when a
+    // shard is booted, and a READY payload is sent by Discord. This payload
+    // contains data like the current user's guild Ids, current user data,
+    // private channels, and more.
+    //
+    // In this case, just print what the current user's username is.
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "bonkbot",
+    about = "A small silly bot to \"bonk\" people in discord"
+)]
+struct Opt {
+    /// Provide the token
+    #[structopt(short, long)]
+    token: Option<String>,
+    /// Provide the name of a file containing the token
+    #[structopt(short = "f", long)]
+    token_filename: Option<String>,
 }
